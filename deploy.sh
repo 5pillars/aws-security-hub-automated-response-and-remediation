ab4ae2d4748a75bcade2c2745baece63200b92ed
Zultan Holder <zultan@6pillars.io>
2024-06-27 12:11:35 +1000

    Comment out S3 upload

--- deploy.sh
+++ deploy.sh
@@ -23,10 +23,10 @@ chmod +x ./upload-s3-dist.sh
 ./build-s3-dist.sh -b "$playbookName" -v "$versionNumber"
 
 # Synchronize built assets to the specified S3 bucket and version
-aws s3 sync ./global-s3-assets "$bucketName/$versionNumber/"
+#aws s3 sync ./global-s3-assets "$bucketName/$versionNumber/"
 
 # Execute upload script for multiple regions
 regions=(ap-southeast-2 ap-northeast-1 us-east-1 us-east-2 us-west-1 us-west-2 ap-southeast-1 ap-south-1 eu-west-1 eu-west-2)
-for region in "${regions[@]}"; do
-    ./upload-s3-dist.sh "$region"
-done
+#for region in "${regions[@]}"; do
+#    ./upload-s3-dist.sh "$region"
+#done
