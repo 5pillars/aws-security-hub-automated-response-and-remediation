5c85e862d7133f1738bc0232eca2003e2007aa7b
Asitha Jayathilake <asithagihan03@gmail.com>
2025-09-12 11:31:18 +1000

    Fix

--- source/lib/solution_deploy-stack.ts
+++ source/lib/solution_deploy-stack.ts
@@ -8,9 +8,6 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { StringParameter, CfnParameter } from 'aws-cdk-lib/aws-ssm';
 import * as kms from 'aws-cdk-lib/aws-kms';
 import * as fs from 'fs';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as logs from 'aws-cdk-lib/aws-logs';
 import {
   Role,
   CfnRole,
