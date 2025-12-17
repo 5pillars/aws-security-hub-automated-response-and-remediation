0d6e717e3bbb9595243e6a7eabb039f3e893d6ec
Asitha Jayathilake <asithagihan03@gmail.com>
2025-12-17 08:17:47 +0530

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 cd deployment
 chmod +x ./build-s3-dist.sh
-./build-s3-dist.sh -b 5pillars-uat-playbooks -c v2.1.3.6
-./build-s3-dist.sh -b 5pillars-uat-playbooks -v v2.1.3.6
+./build-s3-dist.sh -b 5pillars-prod-playbooks -c v2.1.3.6
+./build-s3-dist.sh -b 5pillars-prod-playbooks -v v2.1.3.6
 chmod +x ./upload-s3-dist.sh
 ./upload-s3-dist.sh ap-southeast-2
 ./upload-s3-dist.sh ap-northeast-1
+./upload-s3-dist.sh ap-east-1
 ./upload-s3-dist.sh us-east-1
 ./upload-s3-dist.sh us-east-2
 ./upload-s3-dist.sh us-west-1
 ./upload-s3-dist.sh us-west-2
 ./upload-s3-dist.sh ap-southeast-1
 ./upload-s3-dist.sh ap-south-1
+./upload-s3-dist.sh ap-south-2
 ./upload-s3-dist.sh eu-west-1
 ./upload-s3-dist.sh eu-west-2
 ./upload-s3-dist.sh me-south-1
