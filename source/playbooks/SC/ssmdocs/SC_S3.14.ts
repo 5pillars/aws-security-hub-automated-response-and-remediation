c5054dc1a0a2fc604a5a135903f60112f3cc01e1
Zultan Holder <zultan@6pillars.io>
2025-12-24 16:38:40 +1000

    Add fix

--- source/playbooks/SC/ssmdocs/SC_S3.14.ts
+++ source/playbooks/SC/ssmdocs/SC_S3.14.ts
@@ -3,7 +3,7 @@
 import { Construct } from 'constructs';
 import { ControlRunbookDocument, ControlRunbookProps, RemediationScope } from './control_runbook';
 import { PlaybookProps } from '../lib/control_runbooks-construct';
-import { HardCodedString, StringVariable } from '@cdklabs/cdk-ssm-documents';
+import { AutomationStep, DataTypeEnum, HardCodedString, Output, StringVariable } from '@cdklabs/cdk-ssm-documents';
 
 export function createControlRunbook(scope: Construct, id: string, props: PlaybookProps): ControlRunbookDocument {
   return new ConfigureS3BucketVersioningDocument(scope, id, { ...props, controlId: 'S3.14' });
@@ -17,15 +17,32 @@ class ConfigureS3BucketVersioningDocument extends ControlRunbookDocument {
       remediationName: 'ConfigureS3BucketVersioning',
       scope: RemediationScope.GLOBAL,
       resourceIdName: 'BucketName',
-      resourceIdRegex: String.raw`^(?!.*\.\.)(?!.*\.$)[a-z0-9][a-z0-9\-\.]{1,61}[a-z0-9]$`,
+      resourceIdRegex: String.raw`^arn:(?:aws|aws-cn|aws-us-gov):s3:::([a-z0-9](?:[a-z0-9.-]{1,61}[a-z0-9])?)$`,
       updateDescription: HardCodedString.of('This document configures versioning for an Amazon Simple Storage Service (Amazon S3) bucket.'),
     });
   }
+
+  protected override getExtraSteps(): AutomationStep[] {
+    return [
+      super.getInputParamsStep({
+        versioningState: 'Enabled',
+      }),
+    ];
+  }
+
+  protected override getInputParamsStepOutput(): Output[] {
+    return [{
+      name: 'versioningState',
+      outputType: DataTypeEnum.STRING,
+      selector: '$.Payload.versioningState',
+    }];
+  }
+
   protected override getRemediationParams():  Record<string, any> {
     const params: Record<string, any> = super.getRemediationParams();
 
     params.BucketName = StringVariable.of('ParseInput.BucketName');
-    params.VersioningState = StringVariable.of('ParseInput.VersioningState'); // optional
+    params.VersioningState = StringVariable.of('GetInputParams.versioningState');
 
     return params;
   }
